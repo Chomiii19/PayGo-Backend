90cbc7b57604d6913bee702be5ebd95035a62b1c
Chomiii19 <jomariborines55@gmail.com>
2025-03-17 22:01:36 +0800

    update

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -42,6 +42,12 @@ const signup = catchAsync(async (req, res, next) => {
     savingsBal: 10000,
   });
 
+  user.verificationCode = {
+    code: generateCode(),
+    expiresAt: new Date(Date.now() + 5 * 60 * 1000),
+  };
+  await user.save();
+
   sendCodeVerification(user);
   createSendToken(user.accountNumber, 200, res);
 });
