4f1040a604d1b010df40f62e5d8c81e71ff69bcd
Chomiii19 <jomariborines55@gmail.com>
2025-03-17 19:53:44 +0800

    update

--- src/routes/appRoutes.ts
+++ src/routes/appRoutes.ts
@@ -14,7 +14,7 @@ router
 router.route("/loan/details").get(appController.getLoanDetails);
 router.route("/transaction-history").get(appController.getTransactionHistory);
 router.route("/apply-loan").post(appController.applyLoan);
-router.route("/generate-qrcode").post(appController.generateQRCode);
+router.route("/generate-qrcode").get(appController.generateQRCode);
 router.route("/add-contatcs").patch(appController.addContacts);
 
 export default router;
